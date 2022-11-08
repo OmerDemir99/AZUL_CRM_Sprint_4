1e8cd94f4b15229e8ce17929e78944dcc85b92a3
OmerDemir <samiderin11@gmail.com>
2022-11-08 14:04:13 +0300

    Changed something

--- src/test/java/com/azulcrm/pages/Omer_US_1584_EventPage.java
+++ src/test/java/com/azulcrm/pages/Omer_US_1584_EventPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class HomePage {
+public class Omer_US_1584_EventPage {
 
-    public HomePage(){
+    public Omer_US_1584_EventPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
--- src/test/java/com/azulcrm/step_definitions/Omer_US_1584_EventModule.java
+++ src/test/java/com/azulcrm/step_definitions/Omer_US_1584_EventModule.java
@@ -1,16 +1,16 @@
 package com.azulcrm.step_definitions;
 
-import com.azulcrm.pages.HomePage;
+import com.azulcrm.pages.Omer_US_1584_EventPage;
 import com.azulcrm.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 
 public class Omer_US_1584_EventModule {
 
-    HomePage homePage = new HomePage();
+    Omer_US_1584_EventPage eventPage = new Omer_US_1584_EventPage();
 
     @Then("users click event button")
     public void users_click_event_button() {
-        homePage.eventButton.click();
+        eventPage.eventButton.click();
         BrowserUtils.sleep(2);
     }
 
