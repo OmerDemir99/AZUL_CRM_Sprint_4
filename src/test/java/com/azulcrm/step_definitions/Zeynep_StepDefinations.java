ad544c760076597e0076580a43058c0339d2bdb8
Zeynocool <zeynocool2019@gmail.com>
2022-11-09 19:57:58 +0300

    First Commit Zeynep

---
+++ src/test/java/com/azulcrm/step_definitions/Zeynep_StepDefinations.java
@@ -0,0 +1,8 @@
+package com.azulcrm.step_definitions;
+
+import com.azulcrm.pages.Zeynep_AZUL1583;
+
+public class Zeynep_StepDefinations {
+    Zeynep_AZUL1583 zeynep_azul1583=new Zeynep_AZUL1583();
+
+}
